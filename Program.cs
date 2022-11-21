6cbc9070671a337e18182eacfc731fdde3a08a6b
Rafael Durand <118639629+Rafaelinn@users.noreply.github.com>
2022-11-20 22:37:59 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MapSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Map google = new Map("Coords");
+        }
+    }
+}
